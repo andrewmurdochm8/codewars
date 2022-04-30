92f48ff63263d53acf7f67ed9e24e6a0c3215e3c
andrewmurdochm8 <102765445+andrewmurdochm8@users.noreply.github.com>
2022-04-30 16:48:31 +0100

    Add files via upload

---
+++ Average and return grade.js
@@ -0,0 +1,10 @@
+// Codewars 8kyu Average scores and return grade
+
+function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+  }
