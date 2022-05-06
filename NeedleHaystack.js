f3f76b90bf58b2acd6073600df3e1999daf1ed68
andrewmurdochm8 <102765445+andrewmurdochm8@users.noreply.github.com>
2022-05-06 20:33:34 +0100

    Add files via upload

---
+++ NeedleHaystack.js
@@ -0,0 +1,6 @@
+// Codewars 8kyu Find the needle in a haystack
+
+function findNeedle(haystack) {
+    const pos = haystack.indexOf('needle',haystack)
+    return(`found the needle at position ${pos}`)
+  }
