5a7acc13f05644fb848cb79273a9643eb186eb66
andrewmurdochm8 <102765445+andrewmurdochm8@users.noreply.github.com>
2022-05-19 23:21:09 +0100

    Add files via upload

---
+++ basicmathsoperations.js
@@ -0,0 +1,9 @@
+// Codewars 8kyu Basic Mathematical Operations, apply operation to the given values
+
+function basicOp(operation, value1, value2)
+{
+  if(operation == '+') return value1 + value2
+  if(operation == '-') return value1 - value2
+  if(operation == '*') return value1 * value2
+  if(operation == '/') return value1 / value2
+}
